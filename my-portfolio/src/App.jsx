5a87f4584f3fba12a757cfaa15df6a5a12feaf8f
Fabrice Fouron <fouronfp05@gmail.com>
2025-12-20 15:34:57 -0500

    added images and links to my socials

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -5,14 +5,37 @@ function App() {
     <>
       <div style={{ border: "1px dotted white", width: 250 }}>
         <div style={{ display: "flex", flexDirection: "column" }}>
+          
           <div style={{ justifyItems: "center" }}>
             <img src='/assets/letter_avatar.png' style={{ height: "125px", borderRadius: "100px" }}/>
           </div>
-          <p style={{ marginTop: 15, marginBottom: 15 }}>Links</p>
-          <div style={{display: "flex", flexDirection: "row", justifyContent: "space-evenly"}}>
-            <img src='/assets/github.svg' style={{ height: "42px"}}/>
-            <img src='/assets/linkedin.svg' style={{ height: "40px"}}/>
-            <img src='/assets/instagram.svg' style={{ height: "40px"}}/>
+          
+          <p style={{ marginTop: 15, marginBottom: 15 }}>Social Media</p>
+          
+          <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-evenly" }}>
+            <a href='https://github.com/fabrice-fouron'>
+              <img src='/assets/github.svg' style={{ height: "42px"}}/>
+            </a>
+            <a href='https://www.linkedin.com/in/fabrice-fouron/'>
+              <img src='/assets/linkedin.svg' style={{ height: "40px"}}/>
+            </a>
+            <a href='https://www.instagram.com/_fabrice05_/'>
+              <img src='/assets/instagram.svg' style={{ height: "40px"}}/>
+            </a>
+          </div>
+
+          <p style={{ marginTop: 15, marginBottom: 15 }}>Projects</p>
+
+          <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-evenly" }}>
+            <div>
+              <img src='/assets/TRACKR.png' style={{ height: "80px"}}/>
+              <h3>Trackr</h3>
+            </div>
+
+            <div>
+              <img src='/assets/StFrancoisDeSales.png' style={{ height: "80px"}}/>
+              <h3>Landing Page</h3>
+            </div>
           </div>
         </div>
       </div>
@@ -20,4 +43,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
