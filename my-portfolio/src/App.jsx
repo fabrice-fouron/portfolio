e61f1e3107082f85d428d1e917f85bad055971fe
Fabrice Fouron <fouronfp05@gmail.com>
2025-12-20 00:36:25 -0500

    moved assets to public folder

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -6,13 +6,13 @@ function App() {
       <div style={{ border: "1px dotted white", width: 250 }}>
         <div style={{ display: "flex", flexDirection: "column" }}>
           <div style={{ justifyItems: "center" }}>
-            <img src='/src/assets/letter_avatar.png' style={{ height: "125px", borderRadius: "100px" }}/>
+            <img src='/public/assets/letter_avatar.png' style={{ height: "125px", borderRadius: "100px" }}/>
           </div>
           <p style={{ marginTop: 15, marginBottom: 15 }}>Links</p>
           <div style={{display: "flex", flexDirection: "row", justifyContent: "space-evenly"}}>
-            <img src='/src/assets/github.svg' style={{ height: "42px"}}/>
-            <img src='/src/assets/linkedin.svg' style={{ height: "40px"}}/>
-            <img src='/src/assets/instagram.svg' style={{ height: "40px"}}/>
+            <img src='/public/assets/github.svg' style={{ height: "42px"}}/>
+            <img src='/public/assets/linkedin.svg' style={{ height: "40px"}}/>
+            <img src='/public/assets/instagram.svg' style={{ height: "40px"}}/>
           </div>
         </div>
       </div>
