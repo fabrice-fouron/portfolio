64e03082d9443f149c0f7cd29c920c3695281229
Fabrice Fouron <fouronfp05@gmail.com>
2025-12-20 00:27:52 -0500

    added some logos for my links later

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1 className="text-3xl underline">
-      Website under construction
-    </h1>
+    <>
+      <div style={{ border: "1px dotted white", width: 250 }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div style={{ justifyItems: "center" }}>
+            <img src='/src/assets/letter_avatar.png' style={{ height: "125px", borderRadius: "100px" }}/>
+          </div>
+          <p style={{ marginTop: 15, marginBottom: 15 }}>Links</p>
+          <div style={{display: "flex", flexDirection: "row", justifyContent: "space-evenly"}}>
+            <img src='/src/assets/github.svg' style={{ height: "42px"}}/>
+            <img src='/src/assets/linkedin.svg' style={{ height: "40px"}}/>
+            <img src='/src/assets/instagram.svg' style={{ height: "40px"}}/>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
